b399babf281b642ae46b32505cb0700b85341420
RubyNguyen218 <hongngoc8012@gmail.com>
2025-08-28 13:00:16 +0700

    Hi em, em check xem nhận được chưa nhé

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -2,7 +2,14 @@ import React from "react";
 import "./Header.css";
 
 const Header = () => {
-  return <div></div>;
+  return <div className='header'>
+    <div className="header-contents">
+      <h2>Đặt món ăn ở đây</h2>
+      <p>Lựa chọn từ thực đơn đa dạng với nhiều món ăn hấp dẫn được chế biến từ những nguyên liệu hảo hạng và chuyên môn ẩm thực. Sứ mệnh của chúng tôi là thỏa mãn cơn thèm ăn của bạn và nâng tầm trải nghiệm ăn uống, từng bữa ăn ngon một</p>
+      <button>Xem thực đơn</button>
+    </div>
+
+  </div>;
 };
 
 export default Header;
