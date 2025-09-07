3968eff86d7a4c4d943dd2f8942d0dc1819a9515
chantrang96 <thutrangulis96@gmail.com>
2025-09-07 12:03:25 +0700

    Cập nhật giỏ hàng

--- src/pages/Cart/Cart.jsx
+++ src/pages/Cart/Cart.jsx
@@ -12,12 +12,12 @@ const Cart = () => {
     <div className="cart">
       <div className="cart-items">
         <div className="cart-items-title">
-          <p>Items</p>
-          <p>Title</p>
-          <p>Price</p>
-          <p>Quantity</p>
-          <p>Total</p>
-          <p>Remove</p>
+          <p>Món</p>
+          <p>Tên món</p>
+          <p>Giá</p>
+          <p>Số lượng</p>
+          <p>Tổng</p>
+          <p>Xóa món</p>
         </div>
         <br />
         <hr />
@@ -47,31 +47,31 @@ const Cart = () => {
       </div>
       <div className="cart-bottom">
         <div className="cart-total">
-          <h2>Cart Totals</h2>
+          <h2>Tổng đơn hàng</h2>
           <div>
             <div className="cart-total-detail">
-              <p>Subtotal</p>
+              <p>Tạm tính</p>
               <p>{getTotalCartAmount()} VND </p>
             </div>
             <hr />
             <div className="cart-total-detail">
-              <p>Delivery Fee</p>
+              <p>Phí vận chuyển</p>
               <p>{20000} VND</p>
             </div>
             <hr />
             <div className="cart-total-detail">
-              <b>Total</b>
+              <b>Tổng</b>
               <b>{getTotalCartAmount() + 2} VND</b>
             </div>
           </div>
-          <button>PROCEED TO CHECKOUT</button>
+          <button>Đi đến trang thanh toán</button>
         </div>
         <div className="cart-promocode">
           <div>
-            <p>If you have a promo code, Enter it here</p>
+            <p>Hãy nhập mã khuyến mại vào đây</p>
             <div className="cart-promocode-input">
-              <input type="text" placeholder="promocode" />
-              <button>Submit</button>
+              <input type="text" placeholder="Mã khuyến mại" />
+              <button>Nhập</button>
             </div>
           </div>
         </div>
